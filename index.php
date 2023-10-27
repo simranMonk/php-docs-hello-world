632eb946c7af333aefc8c47bf21e5fc47757fe93
simranMonk <85606727+simranMonk@users.noreply.github.com>
2023-10-27 09:33:13 +0530

    abc

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     $value = $_GET["name"];
     echo "name";
     echo ($value);
-    
+    echo "done";
 //     $conn = sqlsrv_connect($serverName, $connectionOptions);
 //     $tsql= "SELECT * FROM kareena";
 //     $getResults= sqlsrv_query($conn, $tsql);
